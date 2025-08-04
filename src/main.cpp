c46376433685c746c0a4701850082c10833003f8
mateusz <mateusz.wojtczak42@gmail.com>
2025-08-04 12:24:05 +0200

    runmultiple

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,8 @@ int main(int argc, char *argv[])
 
         ServerManager manager(config);
         manager.init_servers();
-        manager.run_single_server();
+        // manager.run_single_server();
+		manager.run_multiple_servers();
 
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
